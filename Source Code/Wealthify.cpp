cdb54c44206bb0e283739f5d54e9dd3207756b4f
Rujul Krishna <rujul.k@gmail.com>
2023-10-16 23:36:42 +0530

    Reinitiated the date update feature

--- Source Code/Wealthify.cpp
+++ Source Code/Wealthify.cpp
@@ -4,12 +4,13 @@
     Rujul Krishna (23070122511)
     Mahesh Kharote (23070122517)
 */
+
 #include <iostream>
 #include <string>
 #include <cstring>
 using namespace std;
 
-typedef struct category // STRUCT USED // Categories of types of payment
+struct category // STRUCT USED // Categories of types of payment
 {
     string food;
     string gadgets;
@@ -26,6 +27,7 @@ class Wealthify // CLASS USED // Main class of the app
         int menu_temp;
         string user_n[2]; // User name, 0 to store the actual one and 1 to store the user input trying to login
         string user_p[2]; // User pass, 0 to store the actual one and 1 to store the user input trying to login
+        int y, m, d;
 
     public:
         void acc_create() // Account creation function added
@@ -86,6 +88,41 @@ class Wealthify // CLASS USED // Main class of the app
             cout << "The current amount of money you have is " << balance << " Rupees" << endl << endl;
         }
 
+        void date_up() // INNOVATIVE FEATURE?? HEHE PLS GIVE MARKS :D // Function to update the months and year and reset the day
+        {
+            d++; // Incrementing the day by one
+
+            int m_max = 31;  // Max days in a month
+
+            if (m == 4 || m == 6 || m == 9 || m == 11) // For Months that only has 30 days
+            {
+                m_max = 30;
+            } 
+            else if (m == 2) // For February month 
+            {
+                if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))  // 29 Days if a Leap year
+                {
+                    m_max = 29;
+                } 
+                else // 28 Days in Feb for a non Leap year
+                {
+                    m_max = 28;
+                }
+            }
+
+            if (d > m_max) // When the days get past newly defined max month value, increment the month
+            {
+                d = 1;
+                m++;
+                
+                if (m > 12) // When the month crosses 12, reset and increment the year
+                {
+                    m = 1;
+                    y++;
+                }
+            }
+        }
+
         void start_app()
         {
             do
@@ -96,12 +133,42 @@ class Wealthify // CLASS USED // Main class of the app
                 cout << "Enter the Password: "; // Take password from the user
                 cin >> user_p[1];
                 cout << endl;
-            } while (user_n[0] != user_n[1]);
+                if (((user_n[0] == user_n[1]) && (user_p[0] == user_p[1])) == 0) // Give error output message
+                {
+                    cout << "Incorrect username or password" << endl;
+                    cout << "Please try again" << endl << endl;
+                }
+
+            } while (((user_n[0] == user_n[1]) && (user_p[0] == user_p[1])) == 0); // Keeps looping until the correct credentials are entered
             
-            cout << "Entered" << endl;
-        }
+            cout << "Username and Password CORRECT" << endl << endl;
+
+            cout << "Welcome to WEALTHIFY: The Smart Budget Tracker" << endl << endl;
+
+            cout << "App initialization started.. Enter the following details: " << endl << endl; // Getting date info
+            cout << "Enter the Year: ";
+            cin >> y;
+            cout << endl;
+            cout << "Enter the Month: ";
+            cin >> m;
+            cout << endl;
+            cout << "Enter the Day: ";
+            cin >> d;
+            cout << endl;
+
+            cout << "You may start entering details for the date " << d << "-" << m << "-" << y << endl << endl;
+
+            /*date_up();
+
+            cout << "You may start entering details for the date " << d << "-" << m << "-" << y << endl << endl;*/
 
         
+        }
+
+        void get_amt()
+        {
+
+        }
 };
 
 int main()
