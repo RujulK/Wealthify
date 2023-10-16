62d333a12ba1bb752f56fe419aaad0b5f88c58c7
Rujul Krishna <rujul.k@gmail.com>
2023-10-16 20:27:17 +0530

    Account creation feature

--- Source Code/Wealthify.cpp
+++ Source Code/Wealthify.cpp
@@ -23,11 +23,28 @@ class Wealthify // CLASS USED // Main class of the app
         int balance;
         int value[50][12][31]; // Array with 50 Years worth of data thaat can be stored, and then 12 months and 31 days
         int user_type; // User type: 0 for Normal Tier user, 1 for Pro Tier user, 2 for Exclusive Tier user
-    
+        string user_n; // User name
+        string user_p; // User pass
+
+
     public:
+
+        void acc_create() // Account creation function added
+        {
+            cout << "Enter the User Name you would like to have: "; // Take user name from the user
+            cin >> user_n;
+            cout << endl;
+            cout << "Enter the Password you would like to have: "; // Take password from the user
+            cin >> user_p;
+            cout << endl;
+        }
+
         Wealthify(void) // DEFAULT CONSTRUCTOR USED // for 0 balance user account creation, and default Normal tier account
         {
             cout << "Normal Tier User Account has been created" << endl << endl;
+            
+            acc_create();
+            
             balance = 0;
             user_type = 0;
         }
@@ -35,6 +52,9 @@ class Wealthify // CLASS USED // Main class of the app
         Wealthify(int a) // SINGLE VARIABLE PARAMETERIZED CONSTRUCTOR // for custom balance account creation and default Normal tier account
         {
             cout << "Normal Tier User Account has been created" << endl << endl;
+
+            acc_create();
+            
             balance = a;
             user_type = 0;
         }
@@ -55,13 +75,21 @@ class Wealthify // CLASS USED // Main class of the app
                 default:
                     cout << "Invalid user creation option" << endl << endl;
             }
+
+            acc_create();
+
             balance = a;
             user_type = b;
         }
 
         void show()
         {
-            cout << "The current amount of money you have is " << balance << " Rupees" << endl;
+            cout << "The current amount of money you have is " << balance << " Rupees" << endl << endl;
+        }
+
+        void start_app()
+        {
+
         }
 
         
@@ -71,5 +99,6 @@ int main()
 {
     Wealthify x(60, 2); // Object created and variables invoked using parameterized constructor; 
     x.show();
+    x.start_app();
     return 0;
 }
